fdd9d923c29714b250812e5e924db08516b35c12
unknown <39314465+BigBlueDraco@users.noreply.github.com>
2024-03-01 17:07:55 +0200

    add: base message routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  findAll() {}
+  @Get('/:id')
+  findOne() {}
+  @Post()
+  create() {}
+}
